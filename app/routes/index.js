57b04cadaf8c9c68045d539c38c23aaa0014e01d
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-06-13 10:41:15 +0300

    added an answer model to display answers of questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,9 +4,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
   },
-
+  
   actions: {
     savequestion3(params) {
       var newQuestion = this.store.createRecord('question', params);
