a8117a5c24f90fda52093b0e00c3924bd6784a4a
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-06-13 11:17:11 +0300

    add a rental attribute on the review model to refer to one of our rental objects

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   question: DS.attr(),
-  additional: DS.attr()
+  additional: DS.attr(),
+  answer: DS.hasMany('answer', { async: true })
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,22 @@
 
-
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      rentals: this.store.findAll('rental'),
-      reviews: this.store.findAll('review')
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
     });
   },
-  
   actions: {
-    savequestion3(params) {
+    saveQuestion3(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
+      this.transitionTo('index');
+    },
+    saveAnswer(params) {
+      var newAnswer= this.store.createRecord('answer', params);
+      newAnswer.save();
       this.transitionTo('index');
     }
   }
